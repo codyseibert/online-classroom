74588ad2f8a882ee3cbd616c29c82b6dd68a1c35
Cody Seibert <cseibert113@gmail.com>
2022-07-22 11:23:28 -0400

    initial setup

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,45 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "../../../server/db/client";
+import { env } from "../../../server/env.mjs";
+
+export const authOptions: NextAuthOptions = {
+  // Include user.id on session
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    DiscordProvider({
+      clientId: env.DISCORD_CLIENT_ID,
+      clientSecret: env.DISCORD_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        name: {
+          label: "Name",
+          type: "text",
+          placeholder: "Enter your name",
+        },
+      },
+      async authorize(credentials, _req) {
+        const user = { id: 1, name: credentials?.name ?? "J Smith" };
+        return user;
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
