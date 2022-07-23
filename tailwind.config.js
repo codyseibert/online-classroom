50d78eb9f26bcced47734be98e26df9a8f429e9d
Nathaniel Rivera <52226518+developerspoot@users.noreply.github.com>
2022-07-23 15:17:50 -0700

    Added color variables to tailWind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      'primary': '#202835',
+      'primaryLight': '#48505f',
+      'primaryDark': '#00000f',
+      'secondary': '#6466e9',
+      'secondaryLight': '#9b94ff',
+      'secondaryDark': '#233cb6',
+      'bgPrimary': '#E1E2E1',
+      'bgSecondary': '#F5F5F6'
+    }
   },
   plugins: [],
 };
