69fc094c3d703ca5ca93f5bd403cce1f628a537e
Luiz Paulo Lindenmaier da Silva <luizpsilva1@kpmg.com.br>
2022-09-14 11:46:48 -0300

    avoid redirecting to blank page with a single button

--- src/components/common/Header/components/LoggedOutSection.tsx
+++ src/components/common/Header/components/LoggedOutSection.tsx
@@ -4,13 +4,14 @@ const LoggedOutSection = ({ signIn }) => {
   return (
     <div className="ml-3 relative">
       <a
-        onClick={signIn}
+        onClick={(e) => signIn('google')}
         href="#"
         className="link-secondary px-3 py-2 rounded-md text-sm font-medium"
         role="menuitem"
         tabIndex={-1}
         id="user-menu-item-2"
-      >Register / Sign in
+      >
+        Register / Sign in
       </a>
     </div>
   );
